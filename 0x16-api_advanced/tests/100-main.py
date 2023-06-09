afb2b3f2a7779d40f45f9ec442d23c82f97857f5
llaurelson <llaurelson@gmail.com>
2023-06-09 10:09:06 +0100

    Update 100-main.py

--- 0x16-api_advanced/tests/100-main.py
+++ 0x16-api_advanced/tests/100-main.py
@@ -5,7 +5,7 @@
 import sys
 
 if __name__ == '__main__':
-    count_words = __import__('100-count').count_words
+    count_words = __import__('100-count.py').count_words
     if len(sys.argv) < 3:
         print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
         print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
